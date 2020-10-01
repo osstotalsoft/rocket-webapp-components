fe1cad77dacb33aee9ecd49b738de418ae7af5d4
Vasile Cuzmin <vcuzmin@totalsoft.ro>
2020-10-01 15:57:36 +0300

    test1

--- src/core/AddButton/AddButton.js
+++ src/core/AddButton/AddButton.js
@@ -4,6 +4,7 @@ import AddIcon from '@material-ui/icons/Add';
 import CustomIconButton from '../IconButton';
 
 
+
 const AddButton = ({ title, onClick, disabled, fontSize, ...rest }) => (
     <CustomIconButton aria-label="Add" onClick={onClick} disabled={disabled} tooltip={title} {...rest}>
         <AddIcon fontSize={fontSize} />
