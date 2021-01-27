e2549f5cc13217570ca4b3d40c32bc7e34fc4d7f
Adela Brezuica <adela.brezuica@TS1370>
2021-01-27 10:39:34 +0200

    return to prettier formatting

--- components/Autocomplete/autocompleteStyle.js
+++ components/Autocomplete/autocompleteStyle.js
@@ -1,55 +1,53 @@
-import { emphasize } from "@material-ui/core/styles/colorManipulator";
-import styles from "../../assets/jss/styles";
-import { theme as defaultTheme } from "../../themes/defaultTheme";
+import { emphasize } from '@material-ui/core/styles/colorManipulator';
+import styles from '../../assets/jss/styles';
+import { theme as defaultTheme } from '../../themes/defaultTheme';
 
 export const selectedColor = (theme, inputSelectedColor) => {
-  return {
-    color: inputSelectedColor
-      ? inputSelectedColor
-      : theme.autoComplete.selectedInputColor,
-  };
-};
+  return { color: inputSelectedColor ? inputSelectedColor : theme.autoComplete.selectedInputColor }
+}
 
-const autocompleteStyle = (theme) => {
-  const { dangerColor, successColor, defaultFont } = styles(theme);
+const autocompleteStyle = theme => {
+  const {
+    dangerColor,
+    successColor,
+    defaultFont
+  } = styles(theme);
 
-  return {
+
+  return ({
     root: {
       flexGrow: 1,
       height: 250,
     },
     input: {
       ...defaultFont,
-      display: "flex",
-      whiteSpace: "nowrap",
-      overflow: "hidden",
-      textOverflow: "ellipsis",
+      display: 'flex',
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis'
     },
     label: {
-      ...defaultFont,
+      ...defaultFont
     },
     labelError: {
-      color: dangerColor[0] + " !important",
+      color: dangerColor[0] + " !important"
     },
     labelSuccess: {
-      color: successColor[0] + " !important",
+      color: successColor[0] + " !important"
     },
     valueContainer: {
-      ...(theme.autoComplete?.valueContainer ||
-        defaultTheme.autoComplete.valueContainer),
-      "& input": {
-        ...(theme.autoComplete?.input || defaultTheme.autoComplete.input),
-      },
+      ...theme.autoComplete?.valueContainer || defaultTheme.autoComplete.valueContainer,
+      '& input': {
+        ...theme.autoComplete?.input || defaultTheme.autoComplete.input
+      }
     },
     chip: {
       margin: `${theme.spacing(0.5)}px ${theme.spacing(0.25)}px`,
     },
     chipFocused: {
       backgroundColor: emphasize(
-        theme.palette.type === "light"
-          ? theme.palette.grey[300]
-          : theme.palette.grey[700],
-        0.08
+        theme.palette.type === 'light' ? theme.palette.grey[300] : theme.palette.grey[700],
+        0.08,
       ),
     },
     noOptionsMessage: {
@@ -57,15 +55,15 @@ const autocompleteStyle = (theme) => {
     },
     singleValue: {
       ...defaultFont,
-      color: theme.palette?.text?.primary || defaultTheme.palette.text.primary,
+      color: theme.palette?.text?.primary || defaultTheme.palette.text.primary
     },
     placeholder: {
       ...defaultFont,
-      position: "absolute",
-      left: 2,
+      position: 'absolute',
+      left: 2
     },
     paper: {
-      position: "absolute",
+      position: 'absolute',
       zIndex: 1,
       marginTop: theme.spacing(),
       left: 0,
@@ -82,18 +80,16 @@ const autocompleteStyle = (theme) => {
       height: theme.spacing(2),
     },
     selectStyles: {
-      singleValue: (base) => ({
+      singleValue: base => ({
         ...base,
-        color:
-          theme.palette?.text?.primary || defaultTheme.palette.text.primary,
+        color: theme.palette?.text?.primary || defaultTheme.palette.text.primary
       }),
-      input: (base) => ({
+      input: base => ({
         ...base,
-        color:
-          theme.palette?.text?.primary || defaultTheme.palette.text.primary,
-      }),
-    },
-  };
+        color: theme.palette?.text?.primary || defaultTheme.palette.text.primary
+      })
+    }
+  })
 };
 
 export default autocompleteStyle;
