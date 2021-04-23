c05eb49a7aef303f94de205579f97084027e49ff
Elena Dumitrescu <edumitrescu@totalsoftostan.onmicrosoft.com>
2021-04-23 12:18:58 +0300

    fix

--- themes/greenTheme.js
+++ themes/greenTheme.js
@@ -48,8 +48,7 @@ const palette = {
     themeShadowColorRGBA: "rgb(39 108 37)",
     roseColor: "#e91e63",
     grayColor: "#999999",
-    blackColor: "#000",
-    dividerColor: "hsla(0,0%,100%,.3)"
+    blackColor: "#000"
   },
   snackbar: {
     infoBgColor: "#ccf1ff",
@@ -187,7 +186,7 @@ export const theme = {
     hoverBgColor: "rgba(0, 0, 0, 0.08)",
     hoverTextColor: palette.text.primary,
     selectedBgColor: "rgba(0, 0, 0, 0.14)",
-    selectedTextColor: palette.text.primary,
+    selectedTextColor: palette.text.primary
   },
   card: {
     height: "auto",
