9ac34472321230fd735f50a7aa0cb5f9c3d4259f
Andra Sava <andra.sava@totalsoft.ro>
2021-09-22 13:11:30 +0300

    Fixed Could not find the referenced rule "transparent" and "justIcon" in "makeStyles"

--- components/IconButton/iconButtonStyle.js
+++ components/IconButton/iconButtonStyle.js
@@ -1,39 +1,32 @@
 import styles from '../../assets/jss/styles';
 
 const iconButtonStyle = theme => {
-  const {
-    whiteColor,
-    grayColor,
-    defaultFont,
-    buttonColors
-  } = styles(theme);
+  const { whiteColor, grayColor, defaultFont, buttonColors } = styles(theme)
 
   return {
     button: {
       ...defaultFont,
       color: whiteColor,
-      boxShadow:
-        "0 2px 2px 0 rgba(153, 153, 153, 0.14), 0 3px 1px -2px rgba(153, 153, 153, 0.2), 0 1px 5px 0 rgba(153, 153, 153, 0.12)",
-      overflow: "hidden",
-      position: "relative",
-      border: "none",
-      textTransform: "uppercase",
-      willChange: "box-shadow, transform",
-      transition:
-        "box-shadow 0.2s cubic-bezier(0.4, 0, 1, 1), background-color 0.2s cubic-bezier(0.4, 0, 0.2, 1)",
-      display: "inline-block",
-      textAlign: "center",
-      whiteSpace: "nowrap",
-      verticalAlign: "middle",
-      touchAction: "manipulation",
-      cursor: "pointer",
-      userSelect: "none",
-      backgroundImage: "none",
+      boxShadow: '0 2px 2px 0 rgba(153, 153, 153, 0.14), 0 3px 1px -2px rgba(153, 153, 153, 0.2), 0 1px 5px 0 rgba(153, 153, 153, 0.12)',
+      overflow: 'hidden',
+      position: 'relative',
+      border: 'none',
+      textTransform: 'uppercase',
+      willChange: 'box-shadow, transform',
+      transition: 'box-shadow 0.2s cubic-bezier(0.4, 0, 1, 1), background-color 0.2s cubic-bezier(0.4, 0, 0.2, 1)',
+      display: 'inline-block',
+      textAlign: 'center',
+      whiteSpace: 'nowrap',
+      verticalAlign: 'middle',
+      touchAction: 'manipulation',
+      cursor: 'pointer',
+      userSelect: 'none',
+      backgroundImage: 'none',
       backgroundColor: grayColor,
-      "&:hover": {
+      '&:hover': {
         backgroundColor: grayColor,
         boxShadow:
-          "0 14px 26px -12px rgba(153, 153, 153, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(153, 153, 153, 0.2)"
+          '0 14px 26px -12px rgba(153, 153, 153, 0.42), 0 4px 23px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(153, 153, 153, 0.2)'
       }
     },
     primary: buttonColors.primary,
@@ -50,11 +43,13 @@ const iconButtonStyle = theme => {
     warningNoBackground: buttonColors.warningNoBackground,
     dangerNoBackground: buttonColors.dangerNoBackground,
     roseNoBackground: buttonColors.roseNoBackground,
+    transparent: buttonColors.transparent,
     theme: buttonColors.theme,
     themeNoBackground: buttonColors.themeNoBackground,
     themeWithBackground: buttonColors.themeWithBackground,
-    disabled: buttonColors.disabled
-  };
+    disabled: buttonColors.disabled,
+    justIcon: { ...defaultFont }
+  }
 }
 
-export default iconButtonStyle;
+export default iconButtonStyle
